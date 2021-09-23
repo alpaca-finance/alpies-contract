4bc935dee5fa0f0aa94b12547ac88561e0f10569
alpanaca <rblue4594@gmail.com>
2021-09-23 18:59:16 +0700

    feat: prep gang v2 contract

---
+++ contracts/6.12/pricemodels/DescendingStepModel.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+
+pragma solidity 0.6.12;
+
+import "./SafeToken.sol";
+import "../interfaces/IPriceModel.sol";
+
+contract DescendingStepModel is IPriceModel {
+  /// @dev Get current price per token
+  function price() view {
+      return 0;
+  }
+}
