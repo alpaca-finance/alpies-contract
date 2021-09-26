83ffb55f501e2c5e3a221e84702e49230e6d3a8e
alpanaca <rblue4594@gmail.com>
2021-09-26 21:04:26 +0700

    feat: add fix price model for unit test

---
+++ contracts/6.12/pricemodels/FixedPriceModel.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+
+pragma solidity 0.6.12;
+
+import "../utils/SafeToken.sol";
+import "../interfaces/IPriceModel.sol";
+
+contract FixedPriceModel is IPriceModel {
+  /// @dev states
+  uint256 public fixPrice;
+
+  constructor(
+    uint256 _price
+  ) public {
+    fixPrice = _price;
+  }
+
+  /// @dev Get current price per token
+  function price() external view override returns (uint256) {
+    return fixPrice;
+  }
+}
